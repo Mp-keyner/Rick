906f880bbcd3c3a51e8cbfffbcfb6ce81fed8670
mp-keyner <keyneroswaldo0@gmail.com>
2023-08-16 20:59:26 -0500

    16 de agosto 8:59

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
-const Url = 'https://rickandmortyapi.com/api/character'
+
 const Main = document.getElementById('root')
 const root = document.documentElement;
 const btn = document.getElementById('btn')
 let estado = false
 const after = document.getElementById('after')
-const before = document.getElementById('befo')
+const before = document.getElementById('before')
+let NumberPage = 1
 
 
 btn.addEventListener('click', () =>{
@@ -20,34 +21,49 @@ btn.addEventListener('click', () =>{
     estado = true;
     }
 })
-
-fetch(Url).then(response => {
+function GetInfo(NumberPage) {
+    const Url = `https://rickandmortyapi.com/api/character/?page=${NumberPage}`
+fetch(Url)
+.then(response => {
     return response.json()
 })
 .then(data => {
-    console.log(data);
-    const Info = data.results
-    console.log(Info);
-    Info.map(element => {
-        Main.innerHTML += `
-        <div class="Car">
-        <h1>${element.name}</h1> <br> 
-        <img class="img" src="${element.image}"
-        />
-        </div>
-        `
-    }); 
+        console.log(NumberPage)
+        console.log(data);
+        const Info = data.results
+        console.log(Info);
+        Main.innerHTML = ''; 
+Info.map(element => {
+  Main.innerHTML += `
+    <div class="Car">
+      <h1>${element.name}</h1> <br> 
+      <img class="img" src="${element.image}" />
+    </div>
+  `;
+});
+
 })
 .catch(err => {
     console.log(err)
 
 });
+}
 
 
 before.addEventListener('click', () => {
-    console.log('before')
-})
+      NumberPage++;
+      GetInfo(NumberPage);
+      btn.scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  after.addEventListener('click', () => {
+      NumberPage--;
+      GetInfo(NumberPage);
+      btn.scrollIntoView({ behavior: 'smooth' });
+  });
+  
 
-after.addEventListener('click', () => {
-    console.log('after')
-})
+GetInfo(NumberPage)
+
+
+Main.innerHTML = 'hello world'
